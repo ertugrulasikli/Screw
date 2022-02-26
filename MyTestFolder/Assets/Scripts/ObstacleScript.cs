a2943d63eac86e9eb8560743cc3b5cb35018573f
ertugrulasikli <ertugrulasikli@gmail.com>
2022-02-26 04:55:59 +0300

    Adding obstacle
    I didnt make collisionEnter() method exactly

---
+++ MyTestFolder/Assets/Scripts/ObstacleScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject obstacle;
+
+    [SerializeField]
+    private Rigidbody rb;
+
+    void FixedUpdate()
+    {
+        
+    }
+}
